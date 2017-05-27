e8f6ac310438f72e527b2c6b544f437f618050fe
CheonJiLee <cjswlddl@naver.com>
2017-05-27 23:03:06 +0900

    volunteer continue layout activity

--- app/src/main/java/com/example/sangh/midasparactice/VolunteerDetailActivity.java
+++ app/src/main/java/com/example/sangh/midasparactice/VolunteerDetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.sangh.midasparactice;
 
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,15 +18,26 @@ public class VolunteerDetailActivity extends AppCompatActivity {
     private TextView vContent;
     private Bitmap vImage;
     private Button vBtn;
+
     private String title;
+    private String contents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donation_details);
 
-        vTitle = (TextView) findViewById(R.id.volunteer_detail_title);
+        vTitle = (TextView) findViewById(R.id.volunteer_detail_title );
         vContent = (TextView)findViewById(R.id.volunteer_detail_content);
         vBtn = (Button) findViewById(R.id.volunteer_detail_btn);
+
+        Intent intent=getIntent();
+        title=intent.getExtras().getString("title");
+        contents = intent.getExtras().getString("contents");
+
+        vTitle.setText(title);
+        vContent.setText(contents);
+        //dContent.setText(contents);
 
     }
 }
